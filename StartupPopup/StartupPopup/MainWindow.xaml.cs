c61187e3b63dbe4f4ee8ccdde3ca09c12b8776c8
je <je@jm.com>
2020-10-10 19:16:44 +0800

    ..

--- StartupPopup/StartupPopup/MainWindow.xaml.cs
+++ StartupPopup/StartupPopup/MainWindow.xaml.cs
@@ -23,24 +23,55 @@ namespace StartupPopup
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string path = "config.ini";
         Dictionary<string, string> configDict = new Dictionary<string, string>();
+
+        List<string> todoList = new List<string>();
+
         List<CheckBox> chkboxList = new List<CheckBox>();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            List<string> todoList = new List<string>();
-            var readTask = File.ReadAllLinesAsync("config.ini");
+            LoadConfigFile();
+
+            for (int i = 0; i < todoList.Count; i++)
+            {
+                CheckBox chkbox = new CheckBox
+                {
+                    Content = todoList[i],
+                    FontSize = 20,
+                    FontWeight = FontWeights.UltraBold,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                chkbox.Checked += Chkbox_Checked;
+                chkboxList.Add(chkbox);
+            }
+
+            listBox.Items.Clear();
+            listBox.ItemsSource = chkboxList;
+        }
+
+        void LoadConfigFile()
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("未找到配置文件");
+                window.Close();
+                return;
+            }
+            var readTask = File.ReadAllLinesAsync(path);
             var dict = readTask.Result;
             for (int i = 0; i < dict.Length; i++)
             {
                 var line = dict[i];
-                if (line != null)
-                    line = line.Trim();
+                line?.Trim();
 
                 var pair = line.Split('=');
-                if (pair != null && pair.Length != 2)
+                if (pair?.Length != 2)
                     continue;
 
                 var k = pair[0];
@@ -53,35 +84,11 @@ namespace StartupPopup
 
                 configDict.Add(k, v);
             }
-
-            //listBox.FontWeight = FontWeights.UltraBlack;
-
-            for (int i = 0; i < todoList.Count; i++)
-            {
-                CheckBox chkbox = new CheckBox
-                {
-                    Content = todoList[i],                    
-                    FontSize = 20,
-                    FontWeight = FontWeights.UltraBold,
-                    VerticalContentAlignment = VerticalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    HorizontalAlignment = HorizontalAlignment.Center                    
-                };
-                chkbox.Checked += Chkbox_Checked;
-                chkboxList.Add(chkbox);
-            }
-
-            listBox.Items.Clear();
-            listBox.ItemsSource = chkboxList;
-
-
-
-
         }
 
         private void Chkbox_Checked(object sender, RoutedEventArgs e)
         {
-            foreach(var chk in chkboxList)
+            foreach (var chk in chkboxList)
             {
                 if (!(bool)chk.IsChecked)
                 {
